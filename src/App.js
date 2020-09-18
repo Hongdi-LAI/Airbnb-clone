7f06cdce4705524ec4c3848a331ce1d14e2a31d9
Hongdi-LAI <69794567+Hongdi-LAI@users.noreply.github.com>
2020-09-18 19:24:13 +0200

    Header Component Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Home from './Home';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Airbnb Clone</h1>
-
+      <Header />
+      < Home />
     {/* Home */}
       {/* Header */}
       {/* Banner */}
