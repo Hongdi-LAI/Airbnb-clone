1d6c5fafd8937c426b69db8d9ac0993ec2b3a183
Hongdi-LAI <69794567+Hongdi-LAI@users.noreply.github.com>
2020-09-19 17:20:10 +0200

    Main Page Setup - Footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from './Home';
 import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      < Home />
+      <Home />
+      <Footer />
     {/* Home */}
       {/* Header */}
       {/* Banner */}
