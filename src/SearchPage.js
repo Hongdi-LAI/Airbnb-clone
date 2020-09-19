47324ba2328fe17d751177f3f61a463ccf1645b5
Hongdi-LAI <69794567+Hongdi-LAI@users.noreply.github.com>
2020-09-19 19:05:56 +0200

    Firebase Deployed

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -29,7 +29,7 @@ function SearchPage() {
             <SearchResults 
                 img = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
                 location = "Private room in center of London"
-                title = "Stay at this spacious Edwardian House"
+                title = "Stay at this Spacious Edwardian House"
                 description = "1 guest · 1 bedroom · 1 bed · 1 shared bathroom · Wifi · Kitchen · Free Parking · Washing Machine"
                 star = {4.73}
                 price = "￡30 / night"
@@ -38,7 +38,7 @@ function SearchPage() {
 
             <SearchResults 
                 img = "https://www.expatkings.com/wp-content/uploads/2018/10/Airbnb-rental-tips.-Hostmaker-1-620x349.jpg"
-                location = "Luxury studio in center of London"
+                location = "Luxury studio in the south of London"
                 title = "Independant Luxury Studio Apartment"
                 description = "2 guests · 3 bedrooms · 3 beds · 2 shared bathrooms · Wifi · Kitchen"
                 star = {4.3}
@@ -48,7 +48,7 @@ function SearchPage() {
 
             <SearchResults 
                 img = "https://a0.muscache.com/im/pictures/b0998d00-e88f-46c0-ae39-2097900641b0.jpg?im_w=960"
-                location = "Beautiful Studio in center of London"
+                location = "Beautiful studio in central London"
                 title = "London Studio Apartments"
                 description = "4 guests · 3 bedrooms · 4 beds · 2 shared bathrooms · Free Parking · Washing Machine"
                 star = {4.9}
@@ -58,7 +58,7 @@ function SearchPage() {
 
             <SearchResults 
                 img = "https://a0.muscache.com/im/pictures/274df76c-89d2-4ec3-9d8c-1e7c9b67029f.jpg?im_w=960"
-                location = "Spacious Room in central London"
+                location = "Spacious room near Buckingham Palace"
                 title = "30 mins to Oxford Street, Excel London"
                 description = "1 guest · 1 bedroom· 1 bed · 2 shared bathrooms · Wifi · Kitchen · Free Parking · Washing Machine"
                 star = {4.1}
